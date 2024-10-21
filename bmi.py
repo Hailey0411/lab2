e54cf66a19352b05283b24341e3a666980933695
MyatThetHmu <MYATTHET.23@ichat.sp.edu.sg>
2024-10-21 15:34:05 +0800

    BMI file

---
+++ bmi.py
@@ -0,0 +1,22 @@
+def calculate_bmi(height,weight):
+    print("Height="+str(height))
+    print("Weight="+str(weight))
+
+    bmi = (weight)/(height*height)
+    print("The BMI value is: ",bmi)
+
+    if bmi < 18.5:
+        {
+      print("Under Weight")
+        }
+
+    elif bmi >=18.5 and bmi <=25.0:
+        {
+    print("Normal Weight")
+        }
+    else:
+        {
+    print("Over Weight")
+        }
+
+calculate_bmi(weight=87, height=1.73)
